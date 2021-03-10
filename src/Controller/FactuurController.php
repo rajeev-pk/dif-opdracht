d565353ce6ee489c20557ad84c5895866c4ca79e
rajeev-pk <rajeevvanappeldorn@gmail.com>
2021-03-10 10:34:58 +0100

    first commit

---
+++ src/Controller/FactuurController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FactuurController extends AbstractController
+{
+    /**
+     * @Route("/factuur", name="factuur")
+     * @Security("is_granted('ROLE_ADMIN') or is_granted('ROLE_BOEK')")
+     */
+    public function index(): Response
+    {
+        return $this->render('factuur/index.html.twig', [
+            'controller_name' => 'FactuurController',
+        ]);
+    }
+}
---
+++ src/Controller/RaporterenController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RaporterenController extends AbstractController
+{
+    /**
+     * @Route("/raporteren", name="raporteren")
+     * @Security("is_granted('ROLE_ADMIN') or is_granted('ROLE_RAPORT')")
+     * 
+     */
+    public function index(): Response
+    {
+        return $this->render('raporteren/index.html.twig', [
+            'controller_name' => 'RaporterenController',
+        ]);
+    }
+}
